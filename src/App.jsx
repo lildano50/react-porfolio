11444796f0f8c59a545a4de38856fba576e87b8f
Daniel Park <danielapark87@gmail.com>
2023-12-31 14:06:16 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header';
+import Nav from './components/Navigation';
+import Project from './components/Project';
+import Footer from './components/Footer';
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Nav />
+            <Project />
+            <Footer />
+        </div>
+    )
+}
