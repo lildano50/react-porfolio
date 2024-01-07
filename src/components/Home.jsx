5001cd014117ceda56893e99abc904a47f2f85f5
Daniel Park <danielapark87@gmail.com>
2024-01-07 17:10:19 -0600

    updated pages to use react router

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+    const linkStyle = { border: '1px black', padding: '5px' };
+
+    return (
+      <div>
+      <h1>Home Page</h1>
+      <p>
+        Filler Text
+      </p>
+    </div>
+      );
+};
+    
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,41 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import App from './App';
+import Error from './components/Error';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import About from './components/About';
+import Project from './components/Project';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/About',
+        element: <About />,
+      },
+      {
+        path: '/Projects',
+        element: <Project />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
