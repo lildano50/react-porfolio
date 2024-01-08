5689852191e0374f3f56b725f9ce28f26f77b36a
Daniel Park <danielapark87@gmail.com>
2024-01-08 16:27:53 -0600

    updates to stylings and page content

---
+++ src/components/Resume.jsx
@@ -0,0 +1,8 @@
+export default function Resume() {
+    return (
+      <div>
+        <img className="resume" src='../src/assets/images/resume_Page_1.jpg'></img>
+        <img className="resume" src='../src/assets/images/resume_Page_2.jpg'></img>
+      </div>
+    );
+  }
